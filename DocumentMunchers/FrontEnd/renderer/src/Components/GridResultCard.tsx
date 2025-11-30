e300ef525292f098eddcdeea2675a3bb255b8021
AJColpitts <ajcolpitts@mta.ca>
2025-11-30 16:25:23 -0400

    Changed default for workspaces
    The program doesn't make a default workspace now

--- DocumentMunchers/FrontEnd/renderer/src/Components/GridResultCard.tsx
+++ DocumentMunchers/FrontEnd/renderer/src/Components/GridResultCard.tsx
@@ -20,8 +20,8 @@ export default function GridResultCard({ title, summary, keywords, relevance, la
         <div className="pdf-icon" aria-hidden>
           <svg width="56" height="56" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="3" y="3" width="14" height="18" rx="2" stroke="#000" strokeWidth="1.6" fill="none" />
-            <path d="M17 7v-4l4 4" stroke="#000" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-            <text x="6" y="18" fontSize="8" fill="#000">PDF</text>
+            {/*<path d="M17 7v-4l4 4" stroke="#000" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" /> */}
+            <text x="5" y="15" fontSize="6" fill="#000">PDF</text>
           </svg>
         </div>
         {typeof relevance === 'number' && (
--- DocumentMunchers/FrontEnd/renderer/src/Components/ListResultCard.tsx
+++ DocumentMunchers/FrontEnd/renderer/src/Components/ListResultCard.tsx
@@ -19,8 +19,8 @@ export default function ListResultCard({ title, summary, relevance, lastOpened, 
         <div className="pdf-icon" aria-hidden>
           <svg width="56" height="56" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="3" y="3" width="14" height="18" rx="2" stroke="#000" strokeWidth="1.6" fill="" />
-            <path d="M17 7v-4l4 4" stroke="#000" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-            <text x="4" y="16" fontSize="7" fill="#000" >PDF</text>
+            {/* <path d="M17 7v-4l4 4" stroke="#000" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" /> */}
+            <text x="5" y="15" fontSize="6" fill="#000" >PDF</text>
           </svg>
         </div>
       </div>
